b97c257a158753cf9805ddce4e47426c8a1f306e
TheKacho <abyssknightzero@gmail.com>
2022-05-21 13:30:18 -0400

    added previous and next page class members

--- Pokedex/Pages/PokemonList.cs
+++ Pokedex/Pages/PokemonList.cs
@@ -44,5 +44,38 @@ namespace Pokedex.Pages
             //this awaits for all tasks and set data model for the page
             Pokemon = (await Task.WhenAll(tasks)).ToList();
         }
+
+        //first value that is overwritten after first API call
+        int TotalPokemon = 932;
+        
+        // determines whether loading spinner for next page is shown or not
+        bool LoadingNextPage = false;
+
+        //determines whether loading spinner for previous page is shown
+        bool LoadingPreviousPage = false;
+        int CurrentPage = 1;
+
+        // this evaluates whether if the user is on the last page of pokemon entries
+        bool OnLastPage() => (CurrentPage * ItemsPerPage) >= TotalPokemon;
+
+
+        // these two methods below are helper methods that are
+        // triggered from previous and next page buttons on UI
+        private async Task LoadNextPage()
+        {
+            LoadingNextPage = true;
+            CurrentPage++;
+            await LoadPage();
+            LoadingNextPage = false;
+        }
+
+        private async Task LoadPreviousPage()
+        {
+            LoadingPreviousPage = true;
+            CurrentPage--;
+            await LoadPage();
+            LoadingPreviousPage = false;
+
+        }
     }
 }
